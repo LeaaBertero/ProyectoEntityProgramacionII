09a804e82be723ea1f074c807aede195b52f3581
Leandro Bertero <leandro.bertero@hotmail.com>
2024-06-06 15:48:44 -0300

    Datos conexion BD

---
+++ TrabajandoEnCapas/Entidades/Profesional.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Profesional
+    {
+        #region atributos
+        private int codProf;
+        private string nombre;
+        #endregion
+
+        #region Constructor
+        public Profesionales()
+        {
+            codProf = 0;
+            //string nombre = ""; 
+            nombre = string.Empty;
+        }
+        #endregion
+
+        #region propiedades/ encapsulamiento
+        public int CodProf 
+        {
+            get { return codProf; }
+            set { codProf = value; }
+        }
+
+        public string Nombre 
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        #endregion
+
+
+    }
+}
