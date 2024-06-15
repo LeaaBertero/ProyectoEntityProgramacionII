d68e9cb1b61ee511e8fdc8c95f36f51cbe1e4971
Leandro Bertero <leandro.bertero@hotmail.com>
2024-06-15 15:48:57 -0300

    entidades

---
+++ TrabajandoEnCapas/Entidades/Profesional.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Profesional
+    {
+        #region atributos
+        private int codProf;
+        private string nombre;
+        #endregion
+
+
+        #region constructor
+        public Profesionales() 
+        {
+            codProf = 0;
+            nombre = string.Empty;
+        }
+        #endregion
+
+        #region propiedades/encapsulamiento
+        public int CodProf
+        {
+            get { return codProf; }
+            set { codProf = value; }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        #endregion
+
+    }
+}
